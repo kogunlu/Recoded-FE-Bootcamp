9254b41cc75c5c965aaad5b04bb00c71f8b6ba0d
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-09-28 14:44:13 +0300

    Tailwind | practise added.

---
+++ My-notes/08_JavaScript/Class_notes/main.js
@@ -0,0 +1,34 @@
+const name = 'Kuday';
+const lastName = 'Ogunlu'
+let age = 24;
+let isStudentInRecoded = true;
+
+
+console.log(name);
+
+
+function helloWorld() {
+    console.log("Hello world!")
+}
+
+function greeting (firstName, lastName2){
+    console.log("Hello " + firstName);
+    console.log(`Surname is: ${lastName2}`);
+}
+
+greeting(Kuday, Ogunlu);
+
+
+
+
+//-----------------------------------------------
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------
